597e3db74ee2afe666a8158e5775a34fe866364c
John Willard <john.willard@colorado.edu>
2019-11-22 15:38:03 -0700

    Fixed Server not listening

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
+let path = require('path');
+
+var port = 3000;
 
 app.get('/', function(req, res) {
-  res.render('/views/index.html');
+  res.sendFile(path.join(__dirname + '/views/index.html'));
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
