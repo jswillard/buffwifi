84af6998c892a1ebf63835895695e38c32bab216
John Willard <john.willard@colorado.edu>
2019-11-22 15:45:39 -0700

    Changed Server Port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 let app = express();
 let path = require('path');
 
-var port = 3000;
+var port = process.env.PORT || 5000;
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
