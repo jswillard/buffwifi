75bf9c90cee09ae8c2c6282e7f8dcf280d21904e
John Willard <john.willard@colorado.edu>
2019-12-04 19:10:26 -0700

    Added Compare

--- server.js
+++ server.js
@@ -24,16 +24,53 @@ let db = pgp(process.env.DATABASE_URL || cn);
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/'));
 
+var buildings = ["Benson Earth Sciences",
+"Clare Small",
+"Cristol Chemistry and Biochemistry",
+"Duane Physics and Astrophysics",
+"Eaton Humanities",
+"Economics",
+"Education",
+"Ekeley Science",
+"Environmental Design",
+"Fleming Law",
+"Gold Biosciences",
+"Guggenheim Geography",
+"Hale Science",
+"Hellems Arts and Sciences",
+"Imig Music",
+"JILA",
+"Ketchum Arts and Sciences",
+"Koelbel (Leeds School of Business)",
+"Mathematics",
+"McKenna Languages",
+"Muenzinger Psychology",
+"Norlin Library",
+"Porter Biosciences",
+"Ramaley Biology",
+"ATLAS Center",
+"Visual Arts Complex",
+"Wolf Law",
+"Museum Collections",
+"Center for Academic Success & Engagement (CASE)",
+"Rec Center",
+"University Memorial Center (UMC)",
+"Center for Community (C4C)",
+"Engineering Center",
+"Integrated Teaching & Learning Laboratory (ITLL)",
+"Discovery Learning Center (DLC)"];
+
+console.log(buildings)
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/views/index.html'));
 })
 
 app.get('/results', function(req, res) {
-  var latitude = req.query.lat;
-  var longitude = req.query.long;
   var upload = req.query.up;
   var download = req.query.down;
   var ping = req.query.ping;
+  var loc = req.query.loc;
 
   res.sendFile(path.join(__dirname + '/views/SpeedTestResults.html'));
 });
